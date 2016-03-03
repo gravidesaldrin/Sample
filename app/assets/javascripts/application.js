fa3bf15e9e668d0eea4b2168fd73f10906ca93c7
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-03 14:56:02 +0800

    Initial Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+$(".alert").addClass("in").fadeOut(4500);
+
+/* swap open/close side menu icons */
+$('[data-toggle=collapse]').click(function(){
+  	// toggle icon
+  	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+});
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root "static_pages#home"
+  get "about" => "static_pages#about"
+  get "contact_us" => "static_pages#contact_us"
+  get "help" => "static_pages#help"
+
+end
